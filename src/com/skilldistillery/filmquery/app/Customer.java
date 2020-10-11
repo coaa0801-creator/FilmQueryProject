2dcdac8a4719646a49fa0a666f69a109a7dfe88d
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-10-10 21:31:17 -0600

    CUSTOMER SEARCH MENU

--- src/com/skilldistillery/filmquery/app/Customer.java
+++ src/com/skilldistillery/filmquery/app/Customer.java
@@ -10,11 +10,11 @@ public class Customer {
 		System.out.println("\n\n\n<================================>");
 		System.out.println("|           SEARCH MENU          |");
 		System.out.println("|                                |");
-		System.out.println("|   1: By Name                   |");
-		System.out.println("|   2: By Rating                 |");
-		System.out.println("|   3: By Category               |");
-		System.out.println("|   4: Main Menu                 |");
-		System.out.println("|   5: Quit                      |");
+		System.out.println("|   1: BY FILM NAME              |");
+		System.out.println("|   2: BY RATING                 |");
+		System.out.println("|   3: BY ACTOR                  |");
+		System.out.println("|   4: BY GENRE                  |");
+		System.out.println("|   5: QUIT                      |");
 		System.out.println("|                                |");
 		System.out.println("<================================>");}
 }
